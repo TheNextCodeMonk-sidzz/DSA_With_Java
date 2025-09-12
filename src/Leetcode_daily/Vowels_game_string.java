e48468db0293e91a7950dbd28f2ea5595c60e7b3
Siddhant Bhujbal <siddhant.devhub101@gmail.com>
2025-09-12 09:16:36 +0530

    Vowels game que Leetcode daily

---
+++ src/Leetcode_daily/Vowels_game_string.java
@@ -0,0 +1,24 @@
+package Leetcode_daily;
+//https://leetcode.com/problems/vowels-game-in-a-string/description/?envType=daily-question&envId=2025-09-12
+public class Vowels_game_string {
+    public boolean doesAliceWin(String s) {
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' ||ch =='o' || ch=='u') return true;
+        }
+        return false;
+    }
+}
+
+//Alice and Bob are playing a game on a string.
+//
+//You are given a string s, Alice and Bob will take turns playing the following game where Alice starts first:
+//
+//On Alice's turn, she has to remove any non-empty substring from s that contains an odd number of vowels.
+//On Bob's turn, he has to remove any non-empty substring from s that contains an even number of vowels.
+//The first player who cannot make a move on their turn loses the game. We assume that both Alice and Bob play optimally.
+//
+//Return true if Alice wins the game, and false otherwise.
+//
+//The English vowels are: a, e, i, o, and u.
